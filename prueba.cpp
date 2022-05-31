86cad05bf02be6fb4d9087a68e7a99b6f0bb95dc
veronicacarbonari <veronica.carbonari@gmail.com>
2022-05-31 15:23:46 -0300

    prueba3

--- prueba.cpp
+++ prueba.cpp
@@ -1,2 +1,16 @@
 //probando commit
-//prueba 2
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+using namespace std;
+
+int main () {
+    
+    cout<<"Probando github desde VSC"<<endl;
+    
+    system("pause");
+
+    return 0;
+}
